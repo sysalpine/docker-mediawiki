c11f11d1d6eeb4a01d7f038ab522f8dfa1044b9d
Paul Trunk <mail@p7k.org>
2019-05-09 15:35:25 +0200

    Add fpm image

---
+++ fpm/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Create Symlink for LocalSettings.php (Better Support for config volumes and k8s configmaps)
+ln -sf /var/www/html/config/LocalSettings.php /var/www/html/LocalSettings.php
+
+# Run DB Updates
+if [ -f /var/www/html/config/LocalSettings.php ]; then
+  php -f /var/www/html/maintenance/update.php
+fi
+
+# Fix file permissions for images/
+chown -R www-data:www-data /var/www/html/images
+
+php-fpm
